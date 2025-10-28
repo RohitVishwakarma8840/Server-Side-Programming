8628582d3848e88bcb3e8ea19c157de68df27206
Rohit <rohit.kushwaha@techstalwarts.com>
2025-10-28 17:16:15 +0530

    adding code

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php 
+
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "businessdb";
+$conn = "";
+
+
+
+
+
+ try{
+    $conn = mysqli_connect($db_server,$db_user ,$db_pass ,$db_name);
+
+ }
+ catch(mysqli_sql_exception){
+    echo "<h2>Could not connect </h2>";
+    }
+
+
+if($conn){
+    echo "Connection Successful";
+}
+// }else{
+//     echo "Could not connect";
+// }
+
+?>
